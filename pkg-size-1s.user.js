9fb2cd5d315193d6b7bdf124df9a30ce8e19f85c
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-07-25 19:51:08 +0800

    fix: Fix pkg-size-1s.user.js script to correctly handle URL changes

--- pkg-size-1s.user.js
+++ pkg-size-1s.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pkg-size-1s
 // @namespace    https://github.com/mefengl
-// @version      0.1.11
+// @version      0.2.1
 // @description  Adds button to NPM package pages direct to pkg-size.dev for npm package size check. It now also works on Github repositories.
 // @author       mefengl
 // @match        https://www.npmjs.com/package/*
@@ -15,13 +15,7 @@
 
   const svgIcon = `<svg viewBox="0 0 24 24" width="1em" height="1em" class="inline"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M21 10V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l2-1.14M16.5 9.4L7.55 4.24"></path><path d="M3.29 7L12 12l8.71-5M12 22V12"></path><circle cx="18.5" cy="15.5" r="2.5"></circle><path d="M20.27 17.27L22 19"></path></g></svg>`;
 
-  const host = window.location.host;
-  const pathname = window.location.pathname.split('/')[2];
-
-  if (host === 'www.npmjs.com') createButtonNPM(pathname);
-  if (host === 'github.com') handleGithub(pathname);
-
-  function createButton(parent, href, className, styles, innerHTML) {
+  const createButton = (parent, href, className, styles, innerHTML) => {
     const anchor = document.createElement('a');
     Object.assign(anchor.style, styles);
     anchor.href = href;
@@ -30,46 +24,64 @@
     parent.appendChild(anchor);
   }
 
-  function createButtonNPM(packageName) {
-    let parent = document.querySelector(".w-third-l");
-    if (parent) {
-      let newElement = parent.lastElementChild?.cloneNode(true);
-      let anchor = newElement?.querySelector("a");
-      if (anchor) {
-        Object.assign(anchor.style, { backgroundColor: '#FFF5DE', color: '#FF7251', borderColor: '#FF7251' });
-        anchor.href = `https://pkg-size.dev/${packageName}`;
-        anchor.innerHTML = `${svgIcon} <strong>Check</strong> Package Size`;
-        parent.appendChild(newElement);
-      }
+  const createButtonNPM = packageName => {
+    const parent = document.querySelector(".w-third-l");
+    const newElement = parent?.lastElementChild.cloneNode(true);
+    const anchor = newElement?.querySelector("a");
+
+    if (anchor) {
+      Object.assign(anchor.style, { backgroundColor: '#FFF5DE', color: '#FF7251', borderColor: '#FF7251' });
+      anchor.href = `https://pkg-size.dev/${packageName}`;
+      anchor.innerHTML = `${svgIcon} <strong>Check</strong> Package Size`;
+      parent?.appendChild(newElement);
     }
   }
 
-  function handleGithub(pathname) {
+  const handleGithub = pathname => {
+    document.querySelector('.package-size-section')?.remove();
+
     const readmeFile = document.querySelector("#readme .markdown-body");
-    if (readmeFile) {
-      const preAndCode = readmeFile.querySelectorAll('pre, code');
-      const installCommands = Array.from(preAndCode).map(element => element.innerText.match(/(npm i|npm install|yarn add|pnpm add)(( -[^ ]+)* ([a-z0-9\-]+))+/gi)).filter(match => match) || [];
-      const packages = installCommands.flat().flatMap(command => command.split(' ').filter(word => !word.startsWith('-') && !['npm', 'i', 'install', 'yarn', 'add', 'pnpm'].includes(word)));
+    const installCommands = [...readmeFile?.querySelectorAll('pre, code')].map(element => element.innerText.match(/(npm i|npm install|yarn add|pnpm add)(( -[^ ]+)* ([a-z0-9\-]+))+/gi)).filter(Boolean) || [];
+    const packages = installCommands.flat().flatMap(command => command.split(' ').filter(word => !word.startsWith('-') && !['npm', 'i', 'install', 'yarn', 'add', 'pnpm'].includes(word)));
 
-      const packageSection = Array.from(document.querySelectorAll(".BorderGrid-row")).find(row => row.querySelector('h2')?.innerText.includes('Packages'));
-      if (packageSection) {
-        const newSection = packageSection.cloneNode(true);
-        newSection.querySelector('h2').innerHTML = 'Package Size';
+    const packageSection = [...document.querySelectorAll(".BorderGrid-row")].find(row => row.querySelector('h2')?.innerText.includes('Packages'));
+    if (packageSection && packages.length) {
+      const newSection = packageSection.cloneNode(true);
+      newSection.classList.add('package-size-section');
+      newSection.querySelector('h2').innerHTML = 'Package Size';
 
-        const content = newSection.querySelector('.BorderGrid-cell');
-        while (content.childElementCount > 1) content.removeChild(content.lastChild);
+      const content = newSection.querySelector('.BorderGrid-cell');
+      while (content.childElementCount > 1) content.removeChild(content.lastChild);
 
-        content.appendChild(document.createElement('div')).className = 'text-small color-fg-muted';
-        content.lastChild.innerHTML = 'Check packages size mentioned in README';
-        const buttonContainer = content.appendChild(document.createElement('div'));
-        buttonContainer.className = 'f6 mt-3';
+      const lastChild = content.appendChild(document.createElement('div'));
+      lastChild.className = 'text-small color-fg-muted';
+      lastChild.innerHTML = 'Check packages size mentioned in README';
 
-        packages.forEach(packageName => {
-          createButton(buttonContainer, `https://pkg-size.dev/${packageName}`, "topic-tag topic-tag-link", { backgroundColor: '#FFF5DE', color: '#FF7251' }, packageName);
-        });
+      const buttonContainer = content.appendChild(document.createElement('div'));
+      buttonContainer.className = 'f6 mt-3';
+      packages.forEach(packageName => createButton(buttonContainer, `https://pkg-size.dev/${packageName}`, "topic-tag topic-tag-link", { backgroundColor: '#FFF5DE', color: '#FF7251' }, packageName));
 
-        packageSection.parentNode.insertBefore(newSection, packageSection.nextSibling);
-      }
+      packageSection.parentNode.insertBefore(newSection, packageSection.nextSibling);
     }
   }
+
+  const onUrlChange = () => {
+    const { host, pathname } = window.location;
+    const packageName = pathname.split('/')[2];
+
+    if (host === 'www.npmjs.com') createButtonNPM(packageName);
+    if (host === 'github.com') handleGithub(pathname);
+  }
+
+  ['pushState', 'replaceState'].forEach(eventType => {
+    history[eventType] = ((original) => function () {
+      const result = original.apply(this, arguments);
+      window.dispatchEvent(new Event(eventType));
+      window.dispatchEvent(new Event('locationchange'));
+      return result;
+    })(history[eventType]);
+  });
+
+  window.addEventListener('locationchange', onUrlChange);
+  onUrlChange();
 })();
